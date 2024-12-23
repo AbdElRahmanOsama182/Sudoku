cf56f8ade8976ebf79ac46646b59ef36cfd9fc3b
AbdElRahman Osama <bodyusama207@gmail.com>
2024-12-23 22:11:02 +0200

    Toggling Selection to be able to deselect

--- frontend/src/components/SudokuCell.vue
+++ frontend/src/components/SudokuCell.vue
@@ -1,67 +1,75 @@
 <script>
 export default {
-  props: {
-    value: {
-      type: Number,
-      default: null
+    props: {
+        value: {
+            type: Number,
+            default: null,
+        },
+        isFixed: {
+            type: Boolean,
+            default: false,
+        },
+        rowIndex: {
+            type: Number,
+            required: true,
+        },
+        colIndex: {
+            type: Number,
+            required: true,
+        },
+        isSelected: {
+            type: Boolean,
+            default: false,
+        },
     },
-    isFixed: {
-      type: Boolean,
-      default: false
+    methods: {
+        selectCell() {
+            if (!this.isFixed) {
+                this.$emit("select-cell", {
+                    row: this.rowIndex,
+                    col: this.colIndex,
+                });
+            }
+        },
+        toggleselectCell() {
+            if (!this.isFixed) {
+                this.$emit("toggle-select-cell", {
+                    row: this.rowIndex,
+                    col: this.colIndex,
+                });
+            }
+        },
     },
-    rowIndex: {
-      type: Number,
-      required: true
-    },
-    colIndex: {
-      type: Number,
-      required: true
-    },
-    isSelected: {
-      type: Boolean,
-      default: false
-    }
-  },
-  methods: {
-    selectCell() {
-      if (!this.isFixed) {
-        this.$emit('select-cell', {
-          row: this.rowIndex,
-          col: this.colIndex
-        });
-      }
-    }
-  }
 };
 </script>
 
 <template>
-  <div
-    class="cell"
-    :class="{ 'isfixed': isFixed, 'selected': isSelected }"
-    @click="selectCell"
-  >
-    <span>{{ value }}</span>
-  </div>
+    <div
+        class="cell"
+        :class="{ isfixed: isFixed, selected: isSelected }"
+        @click="toggleselectCell"
+    >
+        <span>{{ value }}</span>
+    </div>
 </template>
 
 <style scoped>
 .cell {
-  width: 40px;
-  height: 40px;
-  border: 1px solid #ccc;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
+    width: 40px;
+    height: 40px;
+    border: 1px solid #ccc;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
 }
 
 .cell.isfixed {
-  background-color: #f0f0f0; /* Different background color */
-  font-weight: bold; /* Mark fixed cells with bold text */
+    background-color: #f0f0f0; /* Different background color */
+    font-weight: bold; /* Mark fixed cells with bold text */
 }
 
 .cell.selected {
-  background-color: #7f9eb6;
+    background-color: #7f9eb6;
 }
 </style>
